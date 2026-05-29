394985da9eadc45bfcad397543ed2b0fd57c660b
Sam <Samantha.Yengst@gmail.com>
2026-05-29 15:00:16 -0500

    Collect data from user

--- tracker.py
+++ tracker.py
@@ -4,7 +4,13 @@ app = typer.Typer()
 
 @app.command()
 def log():
-    typer.echo("Donation logged!")
+    date = typer.prompt("What was the date of your donation?(YYYY-MM-DD)")
+    location = typer.prompt("Where did you donate?")
+    blood_type = typer.prompt("What is your blood type?")
+    pints = typer.prompt("How many pints did you donate?", default=1.0)
+    # Okay I have all the info, now I need to save it to the database
+    # Handle different error cases here too
+    typer.echo(f"Donation logged: {date}, {location}, {blood_type}, {pints} pints")
 
 @app.command()
 def history():
